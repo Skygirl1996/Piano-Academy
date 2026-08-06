f708d932b18dcd3875b464cc4752282f3551dc60
Skygirl1996 <151428513+Skygirl1996@users.noreply.github.com>
2026-08-06 11:44:22 +0330

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,39 +1,40 @@
-// Firebase SDK imports
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { 
-    getFirestore 
+import {
+    getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
-// Firebase configuration
-
 const firebaseConfig = {
+    apiKey:
+        "AIzaSyAndoh-qko5SsDkvTYDCqlEV9rau1jp7RM",
 
-  apiKey: "AIzaSyAndoh-qko5SsDkvTYDCqlEV9rau1jp7RM",
+    authDomain:
+        "online-piano-academy.firebaseapp.com",
 
-  authDomain: "online-piano-academy.firebaseapp.com",
+    projectId:
+        "online-piano-academy",
 
-  projectId: "online-piano-academy",
+    storageBucket:
+        "online-piano-academy.firebasestorage.app",
 
-  storageBucket: "online-piano-academy.firebasestorage.app",
+    messagingSenderId:
+        "1012777671824",
 
-  messagingSenderId: "1012777671824",
-
-  appId: "1:1012777671824:web:05b403fa3ff105b11c8c15"
-
+    appId:
+        "1:1012777671824:web:05b403fa3ff105b11c8c15"
 };
 
 
-// Initialize Firebase
+const app =
+    initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
 
-
-// Firestore database
-
-const db = getFirestore(app);
+const db =
+    getFirestore(app);
 
 
 export { db };
